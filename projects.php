93a243b5e9903ca4386d1b15463df4f0529aa541
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-15 16:41:13 +0000

    got tags to display in nested foreach

--- projects.php
+++ projects.php
@@ -7,13 +7,17 @@ $query = $db->prepare($sql);
 $query->execute();
 $projects = $query->fetchAll();
 
-$sql = "SELECT tag`.`name` FROM `project`
-        JOIN `project_tag` ON `project_id`=`project`.`id`
-        JOIN `tag` ON `tag_id`=`tag`.`id`;";
+$sql = "SELECT * FROM `tag`;";
+$query = $db->prepare($sql);
+$query->execute();
 $tags = $query->fetchAll();
 
 
 
 foreach($projects as $project) {
-    echo '<div class="work-card"><div class="meta"><img src="" class="photo"><div class="description"><h2>'. $project['title']. '</h2><h4>tags here</h4><p>' . $project['description'] . '</p><p class="read-more"><a href="' . $project['site_url'] . '">Visit Site</a></p></div></div></div>';
+    echo '<div class="work-card"><div class="meta"><img src="" class="photo"><div class="description"><h2>'. $project['title']. '</h2><h4>'; 
+    foreach($tags as $tag) {
+        echo $tag['name'];
+    }
+    echo '</h4><p>' . $project['description'] . '</p><p class="read-more"><a href="' . $project['site_url'] . '">Visit Site</a></p></div></div></div>';
 }
