9a1cff110f39a68b48d616a4333638a8290868fe
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-15 16:49:25 +0000

    if statement to check project_id

--- projects.php
+++ projects.php
@@ -7,17 +7,22 @@ $query = $db->prepare($sql);
 $query->execute();
 $projects = $query->fetchAll();
 
-$sql = "SELECT * FROM `tag`;";
+$sql = "SELECT *, `project_id` FROM `tag`;
+        JOIN `project_tag` ON `tag_id`=`tag`.`id`
+        JOIN `project` ON `project_id`=`project`.`id`";
 $query = $db->prepare($sql);
 $query->execute();
 $tags = $query->fetchAll();
 
-
+var_dump($tags);
 
 foreach($projects as $project) {
     echo '<div class="work-card"><div class="meta"><img src="" class="photo"><div class="description"><h2>'. $project['title']. '</h2><h4>'; 
-    foreach($tags as $tag) {
-        echo $tag['name'];
+    //check project id 
+    if($project['id'] == ) {
+        foreach($tags as $tag) {
+            echo $tag['name'];
+        }
     }
     echo '</h4><p>' . $project['description'] . '</p><p class="read-more"><a href="' . $project['site_url'] . '">Visit Site</a></p></div></div></div>';
 }
