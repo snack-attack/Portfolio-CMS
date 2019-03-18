688ad04dddb1a7a4f456aed9a1f4437cb113dbf2
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-18 13:24:46 +0000

    post vars set and sanitised

---
+++ editProject.php
@@ -0,0 +1,20 @@
+<?php
+
+<?php
+require_once './database.php'
+
+if(!empty($_POST['title'] && !empty($_POST['description'])) {
+    $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+    $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
+    if(!empty($_POST['site_url'])) {
+        $site_url = filter_var($_POST['site_url'], FILTER_VALIDATE_URL);
+    }
+    if(!empty($_POST['code_url'])) {
+        $code_url = filter_var($_POST['code_url'], FILTER_VALIDATE_URL);
+    }
+
+    
+}
+
+
+$sql = "INSERT";
