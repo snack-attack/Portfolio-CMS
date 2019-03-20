04ce0ff6d7864cc8db12d33ea1e863c108555b09
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-20 11:52:54 +0000

    css on admin panel refactored

--- views/admin.view.php
+++ views/admin.view.php
@@ -1,9 +1,38 @@
-<?php
-    require_once 'snippets/header.php';
-?>
-<h1>Welcome to the Admin Panel!</h1>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Audrey Hayes :: Developer</title>
+    <meta name="description" content="A portfolio site showing the work of
+    developer Audrey Hayes">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.png" type="image/png">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/adminPanel.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link
+    href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    rel="stylesheet"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+    crossorigin="anonymous">
+    <link rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+    crossorigin="anonymous">
+</head>
+<body>
+    <!--[if lt IE 7]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+<h1 class="admin-header">Welcome to the Admin Panel!</h1>
     <table>
-        <div class="thead">
+        <div class="title">
             <h3>Projects</h3>
             <a href="./projectForm.php" class="createNew">Create New Project</a>
         </div>
@@ -15,7 +44,7 @@
         </thead>
         <tbody>
             <?php
-                require_once './snippets/projects.table.php';
+                require_once 'projects.table.php';
             ?>
         </tbody>
     </table>
--- views/snippets/header.php
+++ views/snippets/header.php
@@ -12,6 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="favicon.png" type="image/png">
         <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/adminPanel.css">
         <link rel="stylesheet" href="css/styles.css">
         <link
         href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
