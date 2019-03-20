df4ce238a94e0ffb1ffa030fd92bc4c0d67fe2e6
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-20 16:38:43 +0000

    refactored login styles

--- login.php
+++ login.php
@@ -3,22 +3,17 @@
 define('USERNAME', 'audrey');
 define('PASSWORD', '$2y$10$TdIbbxTzrqo.DIgWPHw7t.Q1ZDLzzxHwPw.RBhVHVuIxL/eKoCp3O');
 
-if(!empty($_POST['uname']) && !empty($_POST['psw'])) {
+if (!empty($_POST['uname']) && !empty($_POST['psw'])) {
     $username = $_POST['uname'];
     $password = $_POST['psw'];
     session_start();
-
-    if($username == USERNAME && password_verify($password, PASSWORD)) {
+    if ($username == USERNAME && password_verify($password, PASSWORD)) {
         $_SESSION['loggedIn'] = true;
 
         header('Location: admin.php');
     } else {
-        header('Location: index.html');
+        header('Location: index.php');
     }
-    
 } else {
     echo 'Username or password not provided.';
 };
-
-
-
