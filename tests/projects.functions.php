15be715770530fd88887ff35142e7c57f1efd7b1
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-21 14:43:56 +0000

    shufflePhotoNumbers function tested

--- tests/projects.functions.php
+++ tests/projects.functions.php
@@ -6,6 +6,8 @@ use PHPUnit\Framework\TestCase;
 
 class TestProjectsFunctions extends TestCase
 {
+    const RANGE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47];
+
     public function testAlternateClass_workCardAlt()
     {   
         $count = 3; 
@@ -18,5 +20,16 @@ class TestProjectsFunctions extends TestCase
         $count = 2;
         $class = alternateClass($count);
         $this->assertEquals('work-card', $class);
+    }
+
+    public function testShufflePhotoNumbers()
+    {
+        $range = shufflePhotoNumbers();
+
+        $this->assertInternalType('array', $range);
+
+        $this->assertCount(47, $range);
+        $this->assertNotEquals($range, static::RANGE);
+
     }
 }
